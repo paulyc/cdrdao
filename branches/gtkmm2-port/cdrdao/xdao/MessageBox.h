823d62e543d966e7ee9a49ad30ab61c4db09c779
llanero <llanero@2ee4242e-1aa6-4ea0-8f24-aab5e051b859>
2002-10-04 13:49:10 +0000

    gtkmm2 port. Clean up. Rewrite. HIG compliant. Use GtkResponse

--- branches/gtkmm2-port/cdrdao/xdao/MessageBox.h
+++ branches/gtkmm2-port/cdrdao/xdao/MessageBox.h
@@ -18,6 +18,11 @@
  */
 /*
  * $Log: not supported by cvs2svn $
+ * Revision 1.3  2000/05/01 18:15:00  andreasm
+ * Switch to gnome-config settings.
+ * Adapted Message Box to Gnome look, unfortunately the Gnome::MessageBox is
+ * not implemented in gnome--, yet.
+ *
  * Revision 1.2  2000/02/20 23:34:54  llanero
  * fixed scsilib directory (files mising ?-()
  * ported xdao to 1.1.8 / gnome (MDI) app
@@ -30,51 +35,47 @@
 #ifndef __MESSAGE_BOX_H__
 #define __MESSAGE_BOX_H__
 
-#include <gtk--.h>
-#include <gtk/gtk.h>
-#include <stdarg.h>
-
 class MessageBoxBase : public Gtk::Dialog {
 public:
   MessageBoxBase(Gtk::Window *);
   virtual ~MessageBoxBase();
 
-  void init(const char *type, const char *titel, int askDontShow, int nButtons,
-	    int defaultButton, char *buttons[], va_list);
-
-  int run();
-
-  int dontShowAgain() const;
+  void showAskDontShow(bool ask);
+  bool dontShowAgain() const;
 
 protected:
-  int done_;
-  int doneDefault_;
-
+  Gtk::VBox contents;
+  Gtk::HBox hbox;
+  Gtk::VBox vbox;
+  Gtk::Image image;
+  Gtk::Label primary_text;
+  Gtk::Label secondary_text;
   Gtk::CheckButton *dontShowAgain_;
-
-  Gtk::Button *createButton(const char *name);
-  gint delete_event_impl(GdkEventAny*);
-  void buttonAction(int);
 };
+
+class ErrorMessageBox : public MessageBoxBase {
+public:
+  ErrorMessageBox(Gtk::Window *, const char *title, bool askDontShow,
+    const string primary_text, const string secondary_text = 0);
+};
+
 
 class MessageBox : public MessageBoxBase {
 public:
-  MessageBox(Gtk::Window *, const char *titel, int askDontShow, ...);
-  ~MessageBox();
+  MessageBox(Gtk::Window *, const char *title, bool askDontShow,
+    const string primary_text, const string secondary_text = 0);
 };
 
 class Ask2Box : public MessageBoxBase {
 public:
-  Ask2Box(Gtk::Window *, const char *titel, int askDontShow,
-	  int defaultButton, ...);
-  ~Ask2Box();
+  Ask2Box(Gtk::Window *, const char *title, bool askDontShow,
+    const string primary_text, const string secondary_text = 0);
 };
 
 class Ask3Box : public MessageBoxBase {
 public:
-  Ask3Box(Gtk::Window *, const char *titel, int askDontShow,
-	  int defaultButton, ...);
-  ~Ask3Box();
+  Ask3Box(Gtk::Window *, const char *title, bool askDontShow,
+    const string primary_text, const string secondary_text = 0);
 };
 
 #endif
