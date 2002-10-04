1a8f7eb480fb15eb2bc411436af102530add7cf8
llanero <llanero@2ee4242e-1aa6-4ea0-8f24-aab5e051b859>
2002-10-04 13:49:10 +0000

    gtkmm2 port. Clean up. Rewrite. HIG compliant. Use GtkResponse

--- cdrdao/xdao/MessageBox.cc
+++ cdrdao/xdao/MessageBox.cc
@@ -20,21 +20,41 @@
 #include <stddef.h>
 #include <stdarg.h>
 
-#include <gnome--.h>
+#include <libgnomeuimm.h>
 
 #include "MessageBox.h"
 
 MessageBoxBase::MessageBoxBase(Gtk::Window * win)
+ : contents(false, 24), hbox(false, 12), vbox(false, 12)
 {
-  done_ = 0;
-  doneDefault_ = 0;
-
   dontShowAgain_ = NULL;
+
+  set_has_separator(false);
+
+  contents.set_border_width(12);
+
+  image.set_alignment(0.5, 0.0);
+  primary_text.set_line_wrap(true);
+  primary_text.set_selectable(true);
+  primary_text.set_alignment(0.0, 0.0);
+  secondary_text.set_line_wrap(true);
+  secondary_text.set_selectable(true);
+  secondary_text.set_alignment(0.0, 0.0);
+
+  hbox.pack_start(image, false, false, 0);
+  hbox.pack_start(vbox, false, false, 0);
+  vbox.pack_start(primary_text, true, true, 0);
+  vbox.pack_start(secondary_text, true, true, 0);
+
+  contents.pack_start(hbox, true, true, 0);
+
+  get_vbox()->pack_start(contents);
+  contents.show_all();
 
   if (win != NULL)
     set_transient_for(*win);
-
-  set_position(GTK_WIN_POS_CENTER);
+  else
+    set_position(Gtk::WIN_POS_CENTER);
 }
 
 MessageBoxBase::~MessageBoxBase()
@@ -43,145 +63,21 @@ MessageBoxBase::~MessageBoxBase()
   dontShowAgain_ = NULL;
 }
 
-Gtk::Button *MessageBoxBase::createButton(const char *name)
-{
-  Gnome::StockPixmap *pixmap = NULL;
-  const char *text;
-
-  if (strcmp(name, GNOME_STOCK_BUTTON_OK) == 0) {
-//    pixmap = manage(Gnome::StockPixmap::pixmap_widget(*this, name));
-    pixmap = new Gnome::StockPixmap(name);
-    text = "Ok";
-  }
-  else if (strcmp(name, GNOME_STOCK_BUTTON_CANCEL) == 0) {
-//    pixmap = manage(Gnome::StockPixmap::pixmap_widget(*this, name));
-    pixmap = new Gnome::StockPixmap(name);
-    text = "Cancel";
-  }
-  else if (strcmp(name, GNOME_STOCK_BUTTON_YES) == 0) {
-//    pixmap = manage(Gnome::StockPixmap::pixmap_widget(*this, name));
-    pixmap = new Gnome::StockPixmap(name);
-    text = "Yes";
-  }
-  else if (strcmp(name, GNOME_STOCK_BUTTON_NO) == 0) {
-//    pixmap = manage(Gnome::StockPixmap::pixmap_widget(*this, name));
-    pixmap = new Gnome::StockPixmap(name);
-    text = "No";
-  }
-
-  if (pixmap != NULL)
-//    return Gnome::StockPixmap::pixmap_button(*pixmap, text);
-//    return new Gnome::Stock::Buttons::Button(name);
-    return new Gnome::StockButton(name);
-  else 
-    return new Gtk::Button(name);
-}
-
-void MessageBoxBase::init(const char *type, const char *title, int askDontShow,
-			  int nButtons, int defaultButton, char *buttons[],
-			  va_list args)
+void MessageBoxBase::showAskDontShow(bool ask)
 {
   int i;
   const char *s;
 
-  done_ = 0;
-  doneDefault_ = defaultButton;
-
-  set_title(title);
-
-  Gtk::HButtonBox* bbox = manage(new Gtk::HButtonBox(GTK_BUTTONBOX_SPREAD));
-  bbox->show();
-
-  Gtk::VBox* contents = manage(new Gtk::VBox);
-  contents->show();
-  //contents->set_spacing(5);
-
-  
-  for (i = 1; i <= nButtons; i++) {
-    //Gtk::Button* button = manage(new Gtk::Button(buttons[i - 1]));
-    Gtk::Button* button = manage(createButton(buttons[i - 1]));
-    button->show();
-    button->clicked.connect(bind(slot(this,&MessageBoxBase::buttonAction),i));
-    bbox->add(*button);
-  }
-
-  while ((s = va_arg(args, const char *)) != NULL) {
-    Gtk::HBox* lbox = manage(new Gtk::HBox);
-    lbox->show();
-    Gtk::Label* label = manage(new Gtk::Label(s));
-    label->show();
-    lbox->pack_start(*label, FALSE);
-    contents->pack_start(*lbox, FALSE);
-  }
-
-  if (askDontShow) {
+  if (ask) {
     dontShowAgain_ = new Gtk::CheckButton("Don't show this message again");
     dontShowAgain_->set_active(FALSE);
     dontShowAgain_->show();
 
-    Gtk::HBox* box = manage(new Gtk::HBox);
-    Gtk::Label* label = manage(new Gtk::Label(""));
-
-    label->show();
-    box->show();
-    box->pack_end(*dontShowAgain_, FALSE);
-    contents->pack_start(*label, FALSE);
-    contents->pack_start(*box, FALSE);
+    vbox.pack_start(*dontShowAgain_, true, true, 0);
   }
-
-
-  Gtk::HBox* hcontens = manage(new Gtk::HBox);
-  hcontens->show();
-
-  /*
-  Gnome::Pixmap *typePixmap = manage(new Gnome::Pixmap("gnome-info.png"));
-  if (typePixmap != NULL) {
-    hcontens->pack_start(*typePixmap, FALSE, FALSE, 10);
-    typePixmap->show();
-  }
-  */
-
-  hcontens->pack_start(*contents, TRUE, TRUE, 10);
-  get_vbox()->pack_start(*hcontens, FALSE, FALSE, 10);
-  get_vbox()->show();
-
-  
-  get_action_area()->pack_start(*bbox);
-  get_action_area()->show();
-    
 }
 
-void MessageBoxBase::buttonAction(int act)
-{
-  done_ = act;
-}
-
-gint MessageBoxBase::delete_event_impl(GdkEventAny*)
-{
-  done_ = doneDefault_;
-  return 1;
-}
-
-int MessageBoxBase::run()
-{
-  Gtk::Main *app = Gtk::Main::instance();
-
-  show();
-
-  app->grab_add(*this);
-
-  do {
-    app->iteration();
-  }  while (done_ == 0);
-
-  app->grab_remove(*this);
-
-  hide();
-
-  return done_;
-}
-
-int MessageBoxBase::dontShowAgain() const
+bool MessageBoxBase::dontShowAgain() const
 {
   if (dontShowAgain_ != NULL)
     return dontShowAgain_->get_active() ? 1 : 0;
@@ -189,76 +85,54 @@ int MessageBoxBase::dontShowAgain() const
     return 0;
 }
 
-
-MessageBox::MessageBox(Gtk::Window *win, const char *title,
-		       int askDontShow, ...) : MessageBoxBase(win)
+ErrorMessageBox::ErrorMessageBox(Gtk::Window *win, const char *title, bool askDontShow,
+  const string primary_text, const string secondary_text = 0)
+  : MessageBoxBase(win)
 {
-  va_list args;
-  char *buttons[1];
-
-  buttons[0] = GNOME_STOCK_BUTTON_OK;
-
-  va_start(args, askDontShow);
-
-  init(GNOME_MESSAGE_BOX_INFO, title, askDontShow, 1, 1, buttons, args);
-
-  va_end(args);
+  set_title(title);
+  image.set(Gtk::Stock::DIALOG_ERROR, Gtk::ICON_SIZE_DIALOG);
+  add_button(Gtk::Stock::OK, Gtk::RESPONSE_OK);
+  this->primary_text.set_markup("<b><big>" + primary_text + "</big></b>");
+  this->secondary_text.set_text(secondary_text);
+  showAskDontShow(askDontShow);
 }
 
-
-MessageBox::~MessageBox()
+MessageBox::MessageBox(Gtk::Window *win, const char *title, bool askDontShow,
+  const string primary_text, const string secondary_text = 0)
+  : MessageBoxBase(win)
 {
+  set_title(title);
+  image.set(Gtk::Stock::DIALOG_INFO, Gtk::ICON_SIZE_DIALOG);
+  add_button(Gtk::Stock::OK, Gtk::RESPONSE_OK);
+  this->primary_text.set_markup("<b><big>" + primary_text + "</big></b>");
+  this->secondary_text.set_text(secondary_text);
+  showAskDontShow(askDontShow);
 }
 
-Ask2Box::Ask2Box(Gtk::Window *win, const char *title, int askDontShow,
-		 int defaultButton, ...)
+Ask2Box::Ask2Box(Gtk::Window *win, const char *title, bool askDontShow,
+  const string primary_text, const string secondary_text = 0)
   : MessageBoxBase(win)
 
 {
-  va_list args;
-  char *buttons[2];
-  
-  buttons[0] = GNOME_STOCK_BUTTON_YES;
-  buttons[1] = GNOME_STOCK_BUTTON_NO;
-
-  if (defaultButton < 0 || defaultButton > 2)
-    defaultButton = 0;
-
-  va_start(args, defaultButton);
-
-  init(GNOME_MESSAGE_BOX_QUESTION, title, askDontShow, 2, defaultButton,
-       buttons, args);
-
-  va_end(args);
-  
+  set_title(title);
+  image.set(Gtk::Stock::DIALOG_QUESTION, Gtk::ICON_SIZE_DIALOG);
+  add_button(Gtk::Stock::YES, Gtk::RESPONSE_YES);
+  add_button(Gtk::Stock::NO, Gtk::RESPONSE_NO);
+  this->primary_text.set_markup("<b><big>" + primary_text + "</big></b>");
+  this->secondary_text.set_text(secondary_text);
+  showAskDontShow(askDontShow);
 }
 
-Ask2Box::~Ask2Box()
-{
-}
-
-Ask3Box::Ask3Box(Gtk::Window *win, const char *title, int askDontShow,
-		 int defaultButton, ...)
+Ask3Box::Ask3Box(Gtk::Window *win, const char *title, bool askDontShow,
+  const string primary_text, const string secondary_text = 0)
   : MessageBoxBase(win)
 {
-  va_list args;
-  char *buttons[3];
-  
-  buttons[0] = GNOME_STOCK_BUTTON_YES;
-  buttons[1] = GNOME_STOCK_BUTTON_NO;
-  buttons[2] = GNOME_STOCK_BUTTON_CANCEL;
-
-  if (defaultButton < 0 || defaultButton > 3)
-    defaultButton = 0;
-
-  va_start(args, defaultButton);
-
-  init(GNOME_MESSAGE_BOX_QUESTION, title, askDontShow, 3, defaultButton, buttons, args);
-
-  va_end(args);
-  
-}
-
-Ask3Box::~Ask3Box()
-{
+  set_title(title);
+  image.set(Gtk::Stock::DIALOG_QUESTION, Gtk::ICON_SIZE_DIALOG);
+  add_button(Gtk::Stock::NO, Gtk::RESPONSE_NO);
+  add_button(Gtk::Stock::CANCEL, Gtk::RESPONSE_CANCEL);
+  add_button(Gtk::Stock::YES, Gtk::RESPONSE_YES);
+  this->primary_text.set_markup("<b><big>" + primary_text + "</big></b>");
+  this->secondary_text.set_text(secondary_text);
+  showAskDontShow(askDontShow);
 }
