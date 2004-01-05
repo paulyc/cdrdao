687a57957745745ad5b4683458374c57a52c5db3
poolshark <poolshark@2ee4242e-1aa6-4ea0-8f24-aab5e051b859>
2004-01-05 22:10:21 +0000

    automake/autoconf cleanup

--- branches/gnome2/cdrdao/autogen.sh
+++ branches/gnome2/cdrdao/autogen.sh
@@ -1,24 +1,20 @@
 #! /bin/sh
 # Run this to generate the configure script and unpack needed packages
 
-# This generates the aclocal.m4 file based on configure.in. Manual
-# additions should be added to acinclude.m4
-aclocal
-
-# This generates the Makefile.in files from the corresponding Makefile.am
-automake
-
-# This generates the configure script from configure.in
-autoconf
-
 # This deletes the (old) pccts dir and unpacks the latest version
 if test -e pccts.tar.gz ; then
   rm -rf pccts
-  tar xvzf pccts.tar.gz
+  echo "Unpacking pccts.tar.gz"
+  tar xzf pccts.tar.gz
 fi
 
 # This deletes the (old) scsilib dir and unpacks the latest version
 if test -e scsilib.tar.gz ; then
   rm -rf scsilib
-  tar xvzf scsilib.tar.gz
+  echo "Unpacking scsilib.tar.gz"
+  tar xzf scsilib.tar.gz
 fi
+
+# Calls aclocal, automake, autoconf and al. for you
+echo "Running autoreconf"
+autoreconf
